ce1e57fe7ddd6bd0c12dfb9b3ee773d704dda5e9
Jeffrey Schachtsick <schachtj46@gmail.com>
2025-12-31 22:21:21 -0800

    rotation experiments

--- Foundations/Scenes/Ocean/Ocean.gd
+++ Foundations/Scenes/Ocean/Ocean.gd
@@ -7,12 +7,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	#plane.rotation = helicopter.global_rotation
+	helicopter.global_rotation = plane.global_rotation
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	plane.position.x = plane.position.x + 50.0 * delta
+	#Put some spin on the plane
+	plane.rotation_degrees += 30.0 * delta
+	#plane.position.x = plane.position.x + 50.0 * delta
 	# This is moving slower than plane because scale may be lower than plane.
 	#helicopter.position.x -= helicopter.global_position.x * delta
 	helicopter.position.x -= 50.0 / helicopter.global_scale.x * delta
@@ -20,4 +23,6 @@ func _process(delta: float) -> void:
 	#print("helicopter.scale: ", helicopter.scale)
 	# Actual visual position on screen is global_position.
 	#print("helicopter.global_scale: ", helicopter.global_scale)
+	#print("helicopter.rotation: ", rad_to_deg(helicopter.rotation)) 
+	#print("helicopter.global_rotation: ", rad_to_deg(helicopter.global_rotation))
 	pass
