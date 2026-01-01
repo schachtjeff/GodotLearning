5689d14a2c02ad50375d83c3529feb709ff6d957
Jeffrey Schachtsick <schachtj46@gmail.com>
2025-12-31 22:50:09 -0800

    add the look at using mouse left button click example.

--- Foundations/Scenes/Ocean/Ocean.gd
+++ Foundations/Scenes/Ocean/Ocean.gd
@@ -4,30 +4,35 @@ extends Node2D
 @onready var plane: Sprite2D = $Plane
 @onready var helicopter: Sprite2D = %Helicopter
 
+func _unhandled_input(event: InputEvent) -> void:
+	# Set Target of object example when left click (already assigned) is pressed.
+	if event.is_action_pressed("SetTarget"):
+		plane.look_at(get_global_mouse_position())
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#plane.rotation = helicopter.global_rotation
-	helicopter.global_rotation = plane.global_rotation
+	#helicopter.global_rotation = plane.global_rotation
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#Put some spin on the plane
-	plane.rotation_degrees += 30.0 * delta
+	#plane.rotation_degrees += 30.0 * delta
 	#plane.position.x = plane.position.x + 50.0 * delta
 	# This is moving slower than plane because scale may be lower than plane.
 	#helicopter.position.x -= helicopter.global_position.x * delta
 	#helicopter.position.x -= 50.0 / helicopter.global_scale.x * delta
 	#helicopter.position.y -= 50.0 * delta
-	#helicopter.position.y -= 30.0 * delta
+	helicopter.position.y -= 30.0 * delta
 	# Same as above, just more concise.
-	helicopter.translate(Vector2(30.0, 50.0) * delta)
-	helicopter.global_translate(Vector2(30.0, 50.0) * delta)
+	#helicopter.translate(Vector2(30.0, 50.0) * delta)
+	#helicopter.global_translate(Vector2(30.0, 50.0) * delta)
 	
 	#print("helicopter.scale: ", helicopter.scale)
 	# Actual visual position on screen is global_position.
 	#print("helicopter.global_scale: ", helicopter.global_scale)
 	#print("helicopter.rotation: ", rad_to_deg(helicopter.rotation)) 
 	#print("helicopter.global_rotation: ", rad_to_deg(helicopter.global_rotation))
-	pass
