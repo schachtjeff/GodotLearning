9bd1cb134149c8606fcc2620de2afdf34ac884e8
Jeffrey Schachtsick <schachtj46@gmail.com>
2025-12-31 22:01:25 -0800

    experimentation with position and scale.

--- Foundations/Scenes/Ocean/Ocean.gd
+++ Foundations/Scenes/Ocean/Ocean.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 @onready var plane: Sprite2D = $Plane
-@onready var helicopter: Sprite2D = $Helicopter
+@onready var helicopter: Sprite2D = %Helicopter
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,4 +13,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	plane.position.x = plane.position.x + 50.0 * delta
-	helicopter.position.x -= 25.0 * delta
+	# This is moving slower than plane because scale may be lower than plane.
+	#helicopter.position.x -= helicopter.global_position.x * delta
+	helicopter.position.x -= 50.0 / helicopter.global_scale.x * delta
+	
+	#print("helicopter.scale: ", helicopter.scale)
+	# Actual visual position on screen is global_position.
+	#print("helicopter.global_scale: ", helicopter.global_scale)
+	pass
