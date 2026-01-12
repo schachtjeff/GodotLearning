4c515a748f8cd8f71b3287692cec4f6b82874dbd
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-01-11 19:51:12 -0800

    initialization requirements for Character

--- Scripting/Classes/Character.gd
+++ Scripting/Classes/Character.gd
@@ -9,6 +9,12 @@ var name: String
 var weapon: String
 
 
+func _init(p_health: int, p_name: String, p_weapon: String) -> void:
+	health = p_health
+	name = p_name
+	weapon = p_weapon
+
+
 func print_info() -> void:
 	print("%s health:%d wields:%s" % [
 		name, health, weapon
--- Scripting/Classes/Runner.gd
+++ Scripting/Classes/Runner.gd
@@ -4,14 +4,19 @@ extends EditorScript
 
 # Called when the script is executed (using File -> Run in Script Editor).
 func _run() -> void:
-	var aragorn: Character = Character.new()
-	aragorn.health = 100
-	aragorn.name = "Aragorn"
-	aragorn.weapon = "Bow"
-	aragorn.print_info()
+	var aragorn: Character = Character.new(100, "Aragorn", "Bow")
+	# Commenting out or could be deleted after the initialization
+	#aragorn.health = 100
+	#aragorn.name = "Aragorn"
+	#aragorn.weapon = "Bow"
 	
-	var bart: Character = Character.new()
-	bart.health = 50
-	bart.name = "Bart"
-	bart.weapon = "Sling Shot"
+	
+	var bart: Character = Character.new(50, "Bart", "Sling Shot")
+	# Commenting out or could be deleted after the initialization
+	#bart.health = 50
+	#bart.name = "Bart"
+	#bart.weapon = "Sling Shot"
+	
+	aragorn.print_info()
 	bart.print_info()
+	
