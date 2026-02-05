4f8148fe666174714b6e094162ff42dbbe072686
Jeffrey Schachtsick <schachtj46@gmail.com>
2026-02-05 14:20:49 -0800

    Adding transitions between Main and the Game

--- 2d_Projects/Tappy/Globals/gameManager.gd
+++ 2d_Projects/Tappy/Globals/gameManager.gd
@@ -2,16 +2,35 @@ extends Node
 
 const MAIN = preload("res://Scenes/Main/Main.tscn")
 const GAME = preload("res://Scenes/Game/Game.tscn")
+const SIMPLE_CHANGE = preload("res://Scenes/Changes/SimpleChange.tscn")
+const COMPLEX_CHANGE = preload("res://Scenes/Changes/ComplexChange.tscn")
 
-func load_main_scene() -> void:
-	get_tree().change_scene_to_packed(MAIN)
-	
-func load_game_scene() -> void:
-	get_tree().change_scene_to_packed(GAME)
+var next_scene: PackedScene
+var cx: ComplexChange
 
 # Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-#	pass # Replace with function body.
+func _ready() -> void:
+	# Transition between main to fade to game
+	cx = COMPLEX_CHANGE.instantiate()
+	add_child(cx)
+
+func change_to_next() -> void:
+	if next_scene:
+		get_tree().change_scene_to_packed(next_scene)
+
+func start_transition(to_scene: PackedScene) -> void:
+	next_scene = to_scene
+	cx.play_anim()
+
+func load_main_scene() -> void:
+	start_transition(MAIN)
+	#next_scene = MAIN
+	#get_tree().change_scene_to_packed(SIMPLE_CHANGE)
+	
+func load_game_scene() -> void:
+	start_transition(GAME)
+	#next_scene = GAME
+	#get_tree().change_scene_to_packed(SIMPLE_CHANGE)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
